d6409fee21c592bd355a2fd1758d33b14647f4d2
emilekm2142 <emilm7843@gmail.com>
2019-07-10 19:59:44 +0400

    initial for this branch

--- Config.h
+++ Config.h
@@ -5,9 +5,15 @@
 #define Renderer_OLED
 
 //Do we use SSD1306?
-#define Renderer_SSD1306
+//#define Renderer_SSD1306
 
 //Do we use SSH1106?
-//#define Renderer_SSH1106
+#define Renderer_SSH1106
 
 
+
+ //uncomment if you intend to use a RX or TX pin as a button
+#define USE_TX_RX_AS_GPIO
+//Set buttons	//Set buttons
+#define PIN_DOWN  1	
+#define PIN_UP  3	
